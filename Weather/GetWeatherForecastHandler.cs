1b48936e8f8bf4b2cffbd0694ba6fd8e9d6c6146
Tom Vaidyan <tvaidyan@gmail.com>
2022-08-17 22:50:29 -0500

    initial add

---
+++ Weather/GetWeatherForecastHandler.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using System.Text.Json;
+
+namespace HttpUnitTesting.Weather;
+
+public class WeatherForecastRequest :
+    IRequest<WeatherForecastResponse>
+{
+    // your request params will go here
+}
+
+public class GetWeatherForecastHandler : IRequestHandler<WeatherForecastRequest, WeatherForecastResponse>
+{
+    private readonly HttpClient httpClient;
+    public GetWeatherForecastHandler(IHttpClientFactory httpClientFactory)
+    {
+        httpClient = httpClientFactory.CreateClient();
+    }
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public async Task<WeatherForecastResponse> Handle(WeatherForecastRequest request, CancellationToken cancellationToken)
+    {
+        var httpResponse = await httpClient.GetAsync("http://www.randomnumberapi.com/api/v1.0/random?min=26&max=38&count=10");
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            var results = await httpResponse.Content.ReadAsStringAsync();
+            var forecast = JsonSerializer.Deserialize<int[]>(results)!;
+            var weatherForecastResponse = new WeatherForecastResponse
+            {
+                WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = forecast[index],
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            };
+
+            return await Task.FromResult(weatherForecastResponse);
+        }
+
+        throw new HttpRequestException("Uh-oh!  Bad stuff happened.");
+    }
+}
+
+public class WeatherForecastResponse
+{
+    public IEnumerable<WeatherForecast> WeatherForecasts { get; set; } = new List<WeatherForecast>();
+}
---
+++ Weather/WeatherHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using Moq.Contrib.HttpClient;
+using Xunit;
+
+namespace HttpUnitTesting.Weather;
+public class WeatherHandlerTests
+{
+    [Fact]
+    public async Task Handle_CallsRandomNumberExternalApi()
+    {
+        // Arrange
+        var httpTestingUtility = new HttpTestingUtility(new HttpTestingUtilityParameters
+        {
+            ReturnMessage = new HttpResponseMessage
+            {
+                Content = new StringContent("[100,99,98,97,96,95,94,93,92,91]")
+            }
+        });
+        var sut = new GetWeatherForecastHandler(httpTestingUtility.Factory);
+
+        // Act
+        await sut.Handle(new WeatherForecastRequest(), new CancellationToken());
+
+        // Assert
+        // Did the Handle method call the Random Number API?
+        httpTestingUtility.Handler.VerifyRequest(x => x.RequestUri.AbsoluteUri.Contains("randomnumberapi.com"));
+
+        // Was it a GET request?
+        httpTestingUtility.Handler.VerifyRequest(x => x.Method == HttpMethod.Get);
+    }
+
+    [Fact]
+    public async Task Handle_Returns5DayForecast()
+    {
+        // Arrange
+        var httpTestingUtility = new HttpTestingUtility(new HttpTestingUtilityParameters
+        {
+            ReturnMessage = new HttpResponseMessage
+            {
+                Content = new StringContent("[100,99,98,97,96,95,94,93,92,91]")
+            }
+        });
+        var sut = new GetWeatherForecastHandler(httpTestingUtility.Factory);
+
+        // Act
+        var results = await sut.Handle(new WeatherForecastRequest(), new CancellationToken());
+
+        // Assert
+        Assert.Equal(5, results.WeatherForecasts.Count());
+    }
+
+    [Fact]
+    public async Task Handle_ThrowsErrorWhenRandomNumberApiCallFails()
+    {
+        // Arrange
+        var httpTestingUtility = new HttpTestingUtility(new HttpTestingUtilityParameters
+        {
+            ReturnMessage = new HttpResponseMessage
+            {
+                StatusCode = System.Net.HttpStatusCode.ServiceUnavailable
+            }
+        });
+        var sut = new GetWeatherForecastHandler(httpTestingUtility.Factory);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<HttpRequestException>(async () =>
+        {
+            await sut.Handle(new WeatherForecastRequest(), new CancellationToken());
+        });
+    }
+}
